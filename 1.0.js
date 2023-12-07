e172ffc244d48569047809b5dc448e8963eb08fa
Alex6s <alexis.delfosse01@gmail.com>
2023-12-07 14:28:06 +0100

    test intro js

--- 1.0.js
+++ 1.0.js
@@ -1,1 +1,12 @@
+let age = 25;
+let name ="Alexis";
 
+console.log("hello");
+console.log("My name is " + name);
+console.log("I am " + age);
+
+console.log('10' + '5');
+console.log('10' - '5');
+console.log('10' / '5');
+console.log('10' % '5');
+console.log('10' * '5');
