a48cfe9096a8e3358e6adfea06d11adb8e80b783
Alex6s <alexis.delfosse01@gmail.com>
2023-12-08 11:48:45 +0100

    3.4 min & max

---
+++ 3.4.js
@@ -0,0 +1,6 @@
+//Écrivez un programme qui affichera les éléments minimum et maximum d'un tableau donné.
+
+let tab=[1, 2, 3, 4, 5];
+
+console.log(Math.max(...tab));
+console.log(Math.min(...tab));
